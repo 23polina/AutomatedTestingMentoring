7ab131fd032b26311ea6262c3632eacc9b4c26e0
Palina <novik.pi23@gmail.com>
2026-09-23 11:41:19 +0200

    Split actions into 2 functions

--- standard_library/task_1.py
+++ standard_library/task_1.py
@@ -1,14 +1,20 @@
 import json
+import os
 from datetime import date
 
+def load_people_from_file(file_path):
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"File NOT found {file_path}")
+    else:
+        with open(file_path) as data:
+            return json.load(data)
 
 def filter_people_under18():
-    with open("json_program1.json") as people_data:
-        unpacked_data = json.load(people_data)
-    today_date = date.today()
+    people_data = load_people_from_file("json_program1.json")
+    today = date.today()
     final_result = []
-    for people in unpacked_data:
+    for people in people_data:
         transformed_date = date.fromisoformat(people["birth_date"])
-        if transformed_date > date(today_date.year - 18, today_date.month, today_date.day):
+        if transformed_date > date(today.year - 18, today.month, today.day):
             final_result.append(people)
     return final_result
